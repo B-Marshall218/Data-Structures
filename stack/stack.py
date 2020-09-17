760c737f63d0d2581b2c612736d41cf66509e817
Brian <lawmen218@yahoo.com>
2020-09-16 23:45:17 -0400

    missing ques only

--- stack/stack.py
+++ stack/stack.py
@@ -1,25 +1,56 @@
-"""
+'''
+from singly_linked_list import LinkedList, Node
 A stack is a data structure whose primary purpose is to store and
-return elements in Last In First Out order. 
+return elements in Last In First Out order.
 
 1. Implement the Stack class using an array as the underlying storage structure.
-   Make sure the Stack tests pass.
+Make sure the Stack tests pass.
 2. Re-implement the Stack class, this time using the linked list implementation
-   as the underlying storage structure.
-   Make sure the Stack tests pass.
-3. What is the difference between using an array vs. a linked list when 
-   implementing a Stack?
-"""
+as the underlying storage structure.
+Make sure the Stack tests pass.
+3. What is the difference between using an array vs. a linked list when
+implementing a Stack?'''
+
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+
+#         return self.size
+
+#     def push(self, value):
+#         self.size += 1
+#         self.storage.append(value)
+#         return self.storage
+
+#     def pop(self):
+#         if (self.size) == 0:
+#             return
+#         self.size -= 1
+#         value = self.storage.pop()
+#         return value
+
+from singly_linked_list import LinkedList, Node
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+
+        return self.size
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+
+        return self.storage.remove_tail()
